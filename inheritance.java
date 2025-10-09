f671e955ef6dd4cca9cc5abcdc6a40b38ebf792a
ShipluSaha995 <shiplusaha995@gmail.com>
2025-10-09 13:52:36 +0600

    Inheritance

--- inheritance.java
+++ inheritance.java
@@ -1,1 +1,10 @@
-//Definstion of inheritence: 
+/*Definstion of inheritence: Inheritance is the proccess by which one class acquires the properties and 
+behaiviors of another class
+
+it allows the creation of built upon existing ones, enabling code reusability, extensibility and 
+represantation of real-world hierarchical relationships.
+
+The class that inherits is called the subclass and the class from which it inherits is called
+the supper class
+
+*/
