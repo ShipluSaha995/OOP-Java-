1b47e69125308ae635698e1fb13aa75adaa8d2b9
ShipluSaha995 <shiplusaha995@gmail.com>
2025-10-09 10:35:32 +0600

    Encupsulation

--- Encupsulation.java
+++ Encupsulation.java
@@ -1,32 +1,34 @@
 /*Encupsulation: Encupsulation is the machanism tha binds together code and the data it manipulates and 
-keep both safe from outside interference and misuse */
+keep both safe from outside interference and misuse 
+It is often called “data hiding.”
+*/
 
 class Details {
     private String name;
     private int id;
     private String dept;
 
-    public void setName(String name){
+    public void setName(String name){ //setter method
         this.name=name;
     }
 
-    public String getName(){
+    public String getName(){ //getter method
         return name;
     }
 
-    public void setId(int id){
+    public void setId(int id){ //setter method
         this.id=id;
     }
 
-    public int getId(){
+    public int getId(){  //getter method
         return id;
     }
 
-    public void setDept(String dept){
+    public void setDept(String dept){ //setter method
         this.dept=dept;
     }
 
-    public String getDept(){
+    public String getDept(){  //getter method
         return dept;
     }
 
