827bf69bed00f26516ba3ad1666533750542107c
ShipluSaha995 <shiplusaha995@gmail.com>
2025-10-11 10:51:31 +0600

    Encupsulation

--- Encupsulation.java
+++ Encupsulation.java
@@ -1,4 +1,4 @@
-/*Encupsulation: Encupsulation is the machanism tha binds together code and the data it manipulates and 
+/*Encupsulation: Encupsulation is the machanism that binds together code and the data it manipulates and 
 keep both safe from outside interference and misuse 
 It is often called “data hiding.”
 */
