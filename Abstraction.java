82f42d154785584b6603d43f4fed87549de40d66
ShipluSaha995 <shiplusaha995@gmail.com>
2025-10-15 20:29:35 +0600

    Abstraction

---
+++ Abstraction.java
@@ -0,0 +1,12 @@
+/* Abstraction is the proccess of hiding internal implementation details and showing only the essential featuers of an object */
+
+class Faculty{
+    String degree;
+    int Experience;
+
+
+    Faculty(String degree,  int Experience){
+        this.degree=degree;
+        this.Experience=Experience;
+    }
+}
