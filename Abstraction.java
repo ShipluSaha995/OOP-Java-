ed89d2e6df66f4df9838a423650efae2e6bd2f99
ShipluSaha995 <shiplusaha995@gmail.com>
2025-10-15 20:50:52 +0600

    Abstraction

--- Abstraction.java
+++ Abstraction.java
@@ -1,12 +1,50 @@
 /* Abstraction is the proccess of hiding internal implementation details and showing only the essential featuers of an object */
 
-class Faculty{
+abstract class Faculty{
     String degree;
-    int Experience;
+    int experience;
 
 
-    Faculty(String degree,  int Experience){
-        this.degree=degree;
-        this.Experience=Experience;
+    Faculty(){
+        experience=0;
+        degree="Unknown";
+    }
+
+    abstract void teach();
+}
+
+class ChairPerson extends Faculty{
+    ChairPerson(){
+        experience=20;
+        degree="Phd";
+    }
+
+    @Override
+    void teach(){
+        System.out.println("Chair-person teaching.");
+    }
+}
+
+class ProgramCordinator extends Faculty{
+    ProgramCordinator(){
+        experience=10;
+        degree="Masters";
+    }
+
+    @Override
+    void teach(){
+        System.out.println("Program Co-ordinator Teaching.");
+    }
+}
+
+public class Abstraction{
+    public static void main(String[] args) {
+        Faculty fac1 = new ChairPerson();
+        Faculty fac2 = new ProgramCordinator();
+        ChairPerson chair = new ChairPerson();
+
+        fac1.teach();
+        fac2.teach();
+        chair.teach();
     }
 }
