a7494c15a528d1689eede3ad7764c5790cfceae7
ShipluSaha995 <shiplusaha995@gmail.com>
2025-10-23 12:52:24 +0600

    Practice

--- practice.java
+++ practice.java
@@ -1,2 +1,29 @@
-//Implement multiple inheritanece. 3 classes A,B,C and Class C inherits both A and B
+/*Implement multiple inheritanece. 3 classes A,B,C and Class C inherits both A and B
+*/
 
+interface A{
+    void code();
+}
+
+interface B{
+    void language();
+}
+
+class C implements A, B{
+    public void code(){
+        System.out.println("code with");
+    }
+
+    public void language(){
+        System.out.println("Java");
+    }
+}
+
+public class practice{
+    public static void main(String[] args) {
+        C java = new C();
+        java.code();
+        java.language();
+    }
+
+}
