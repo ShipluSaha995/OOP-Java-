5e12bcdbfd23acec9cba35c6011086f1b6e52160
ShipluSaha995 <shiplusaha995@gmail.com>
2025-12-05 08:32:28 +0600

    Call by value

--- Call_by_Value_and_Call_by_Reference.java
+++ Call_by_Value_and_Call_by_Reference.java
@@ -1,1 +1,25 @@
-          =============== COMING SOON ================   
+/*call by Value
+    when we  pass a primitive data types like int, float, char boolean ets to a method 
+    java passes a copy of the value
+
+    changing the value inside the method doesn't affect the original variable
+
+*/
+
+class Example {
+    void change(int x){
+        x=30;
+    }
+}
+
+public class Call_by_Value_and_Call_by_Reference{
+    public static void main(String[] args){
+        Example ex = new Example();
+
+        int a=10;
+        ex.change(a);
+        System.out.println(a);
+    
+
+    }
+}
