5dc3dcefbfac3ad7b8b926c57c5091fa204886bf
ShipluSaha995 <shiplusaha995@gmail.com>
2025-12-11 19:10:48 +0600

    Interface

--- Interface.java
+++ Interface.java
@@ -18,3 +18,69 @@ Interfaces are used :
 
 */
 
+/*interface Camera{
+    void takePhoto();
+}
+
+interface MediaPlayer{
+    void PlayMusic();
+}
+
+class Mobile implements Camera, MediaPlayer{
+    public void takePhoto(){
+        System.out.println("Taking Photo");
+    }
+
+    public void PlayMusic(){
+        System.out.println("Playing Music");
+    }
+}
+
+public class Interface{
+    public static void main(String[] args) {
+        Mobile M = new Mobile();
+        M.PlayMusic();
+        M.takePhoto();
+    }
+}*/
+
+
+interface Camera{
+    void takePhoto();
+}
+
+interface Wifi{
+    void connect();
+}
+
+class Device{
+    void power(){
+        System.out.println("Power on");
+    }
+}
+
+class SmartDevice extends Device{
+    void assistant(){
+        System.out.println("Voice Assistant.");
+    }
+}
+
+class Phone extends SmartDevice implements Camera, Wifi{
+    public void takePhoto(){
+        System.out.println("Taking Photo.");
+    }
+
+    public void connect(){
+        System.out.println("Connected to the Wifi.");
+    }
+}
+
+public class Interface{
+    public static void main(String[] args) {
+        Phone ph = new Phone();
+        ph.power();
+        ph.assistant();
+        ph.connect();
+        ph.takePhoto();
+    }
+}
